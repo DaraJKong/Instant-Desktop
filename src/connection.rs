cc0023169d4247fd77dbf84164106eeff863b9aa
DaraJKong <daraetkong@gmail.com>
2023-09-07 15:14:03 -0400

    Add connection module

---
+++ src/connection.rs
@@ -0,0 +1,74 @@
+// Copyright 2023 Dara Kong
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use directories::ProjectDirs;
+use lazy_regex::{regex, regex_replace_all};
+use std::{fs, path::Path, process::Command};
+
+pub fn start_rdc_session(base_config_path: &Path, selected_monitors: Vec<u32>) {
+    // read base file
+    let mut rdp_config =
+        fs::read_to_string(base_config_path).expect("failed to read base config file");
+
+    // use_multimon parameter
+    let use_multimon_r = regex!(r#"^(use\smultimon:i:).*$"#im);
+
+    if use_multimon_r.is_match(&rdp_config) {
+        rdp_config = regex_replace_all!(
+            r#"^(use\smultimon:i:).*$"#im,
+            &rdp_config,
+            |_, key| format!("{}1", key),
+        )
+        .to_string();
+    } else {
+        rdp_config.push_str("\nuse multimon:i:1");
+    }
+
+    // selectedmonitors parameter
+    let selected_value = selected_monitors
+        .iter()
+        .map(|id| id.to_string())
+        .collect::<Vec<String>>()
+        .join(",");
+
+    let selectedmonitors_r = regex!(r#"^(selectedmonitors:s:).*$"#im);
+
+    if selectedmonitors_r.is_match(&rdp_config) {
+        rdp_config = regex_replace_all!(
+            r#"^(selectedmonitors:s:).*$"#im,
+            &rdp_config,
+            |_, key| format!("{}{}", key, selected_value),
+        )
+        .to_string();
+    } else {
+        rdp_config.push_str(&format!("\nselectedmonitors:s:{}", selected_value));
+    }
+
+    // write custom file
+    if let Some(project_dirs) = ProjectDirs::from("", "", "Instant-Desktop") {
+        let mut path = project_dirs.data_dir().to_path_buf();
+        path.push("custom.rdp");
+
+        if fs::write(&path, rdp_config).is_ok() {
+            // start remote desktop connection
+            let path_arg = format!("\"{}\"", path.to_str().unwrap());
+
+            Command::new("mstsc").args(["/edit", &path_arg]);
+        } else {
+            panic!("failed to write custom config file");
+        }
+    } else {
+        panic!("failed to get project directories");
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+pub mod connection;
 pub mod palette;
 pub mod windows;
 
